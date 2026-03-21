c637e4a62e9c1bea59a4e05804d5bb7568503899
Artyom Ivanov <zuodleq@gmail.com>
2026-03-21 13:00:45 +0300

    Add simple command line interface

--- main.cpp
+++ main.cpp
@@ -3,21 +3,34 @@
 
 #include "parser.h"
 
+class TUI {
+public:
+    void run()
+    {
+        while (true) {
+            std::string input;
+            std::cout << "> ";
+            std::getline(std::cin, input);
+            if (input.empty()) {
+                continue;
+            }
+
+            try {
+                ast::NodePtr ast = ast::parser::parse(input);
+                std::string outputFile;
+
+                std::cout << "Filename of .dot: ";
+                std::getline(std::cin, outputFile);
+                dumpToGraphviz(outputFile, ast);
+            } catch (const std::exception & e) {
+                std::cerr << e.what();
+            }
+        }
+    }
+};
+
 int main(int argc, char const * argv[])
 {
-    std::string input;
-    std::cout << "> ";
-    std::getline(std::cin, input);
-
-    try {
-        ast::NodePtr ast = ast::parser::parse(input);
-        std::string outputFile;
-
-        std::cout << "Filename of .dot: ";
-        std::getline(std::cin, outputFile);
-        dumpToGraphviz(outputFile, ast);
-    } catch (const std::exception& /* ex */) {
-        // Temporary solution
-        throw std::runtime_error("Parse error!");
-    }
+    TUI tui;
+    tui.run();
 }
